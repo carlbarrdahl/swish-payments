4eff8fbb45106f0a0725009f07cf11d7f2f0758d
Carl Barrdahl <carlbarrdahl@gmail.com>
2018-05-15 10:58:51 +0200

    Fix tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,7 +13,7 @@ describe("Swish payment", () => {
   it("handles a payment requests", async () => {
     const token = await swish.paymentRequest({
       message: "",
-      callbackUrl: "https://example.com/paymentrequestss",
+      callbackUrl: "https://example.com/paymentrequests",
       amount: "100.00",
       currency: "SEK",
       payeeAlias: "1231181189",
@@ -28,20 +28,20 @@ describe("Swish payment", () => {
     expect(payment.status).toBe("CREATED")
   })
 
-  // it("handles Swish callback", () => {
-  //   const mockPayment = {
-  //     amount: "100.00",
-  //     customer: "foo"
-  //   }
-  //   const mock = {
-  //     callback: jest.fn(),
-  //     req: { body: mockPayment, connection: { remoteAddress: "194.242.111.220:443" } },
-  //     res: { status: jest.fn() }
-  //   }
-  //   const hook = swish.createHook(mock.callback)
-  //   hook(mock.req, mock.res)
+  it("handles Swish callback", () => {
+    const mockPayment = {
+      amount: "100.00",
+      customer: "foo"
+    }
+    const mock = {
+      callback: jest.fn(),
+      req: { body: mockPayment, connection: { remoteAddress: "194.242.111.220:443" } },
+      res: { status: jest.fn() }
+    }
+    const hook = swish.createHook(mock.callback)
+    hook(mock.req, mock.res)
 
-  //   expect(mock.callback).toHaveBeenCalledWith(mockPayment)
-  //   expect(mock.res.status).toHaveBeenCalled()
-  // })
+    expect(mock.callback).toHaveBeenCalledWith(mockPayment)
+    expect(mock.res.status).toHaveBeenCalledWith(201)
+  })
 })
